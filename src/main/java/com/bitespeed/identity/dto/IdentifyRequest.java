8ba00de539d675234714e9d525c8c5279926d148
manas541 <purohitmanas540@gmail.com>
2026-02-27 15:30:32 +0530

    Logic Creation with Service and Coontroller Layer

--- src/main/java/com/bitespeed/identity/dto/IdentifyRequest.java
+++ src/main/java/com/bitespeed/identity/dto/IdentifyRequest.java
@@ -1,22 +1,10 @@
 package com.bitespeed.identity.dto;
 
+
+import lombok.Data;
+
+@Data
 public class IdentifyRequest {
     private String email;
     private String phoneNumber;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
